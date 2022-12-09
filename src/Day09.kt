187fdaac1a019b0c8d42e24db572da274a6c8e3b
Teng Zhang <zhangt2333@gmail.com>
2022-12-09 15:08:14 +0800

    feat: day09

---
+++ src/Day09.kt
@@ -0,0 +1,66 @@
+import kotlin.math.abs
+import kotlin.math.sign
+
+data class Point(
+    var x: Int,
+    var y: Int
+) {
+    fun move(char: Char) {
+        when (char) {
+            'U' -> y++
+            'D' -> y--
+            'L' -> x--
+            'R' -> x++
+        }
+    }
+
+    fun follow(head: Point) {
+        val dx = head.x - x
+        val dy = head.y - y
+        if (abs(dx) > 1) {
+            x += dx - dx.sign
+            if (abs(dy) == 1) y += dy.sign
+        }
+        if (abs(dy) > 1) {
+            y += dy - dy.sign
+            if (abs(dx) == 1) x += dx.sign
+        }
+    }
+}
+
+fun main() {
+
+    fun solve(input: List<String>, ropeSize: Int): Int {
+        val visited = mutableSetOf<Point>()
+        val ropes = Array(ropeSize) { Point(0, 0) }
+        visited += ropes.last().copy()
+        for (line in input) {
+            val direction = line[0]
+            val step = line.substring(2).toInt()
+            repeat(step) {
+                for ((i, rope) in ropes.withIndex()) {
+                    if (i == 0) rope.move(direction)
+                    else rope.follow(ropes[i - 1])
+                }
+                visited += ropes.last().copy()
+            }
+        }
+        return visited.size
+    }
+
+    fun part1(input: List<String>): Int {
+        return solve(input, 2)
+    }
+
+    fun part2(input: List<String>): Int {
+        return solve(input, 10)
+    }
+
+    check(part1(readLines("Day09_test")) == 13)
+    check(part2(readLines("Day09_test")) == 1)
+    check(part2(readLines("Day09_test2")) == 36)
+
+    val input = readLines("Day09")
+    println(part1(input))
+    println(part2(input))
+}
